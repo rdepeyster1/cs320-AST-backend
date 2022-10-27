468ec798b07da82d80a33a6407d4912de44c0016
Laurent Man <laurent.tian.man@gmail.com>
2022-10-27 02:40:48 -0400

    Fixed some bugs in CREATE API

--- routes/api.js
+++ routes/api.js
@@ -16,8 +16,10 @@ async function queryDb (query, values) {
   let r = await pool.request();
   (values || []).forEach((v, i) => r.input(`_${i + 1}`, v));
   let data = await r.query(query);
-  for (let i=0;i<data.rowsAffected;i++){ 
-    res.push(data.recordset[i]); 
+  if (data.recordset){
+    for (let i=0;i<data.rowsAffected;i++){ 
+      res.push(data.recordset[i]); 
+    }
   }
   pool.close; sql.close; 
   return res; 
@@ -37,27 +39,38 @@ router.get("/goals/get/:id", (req, res, next) => {
   })
 });
 
-queryDb("SELECT MAX(goalid) as mgi from Goals").then(result=>{console.log(result)})
-
 router.post("/goals/create", (req, res, next) => {
+  const empid = parseInt(req.body.empid);
+  const startdate = req.body.startdate;
+  const enddate = req.body.enddate;
+  const description = req.body.description;
+  const goaltype = req.body.goaltype;
+  const status = req.body.status;
   queryDb("SELECT MAX(goalid) as mgi from Goals")
       .then(result=>{
-        const goalid = result.mgi + 1;
-        const empid = req.params.empid;
-        const startdate = req.params.startdate;
-        const enddate = req.params.enddate;
-        const description = req.params.description;
-        const goaltype = req.params.goaltype;
-        const status = req.params.status;
-
+        const goalid = result[0].mgi + 1;
         const goalinfo = [goalid, empid, startdate, enddate, description, goaltype, status];
+        
+        console.log(goalinfo);
+        
+        queryDb("INSERT INTO GOALS VALUES(@_1, @_2, @_3, @_4, @_5, @_6, @_7)", goalinfo);
+      })
+      .catch(err=>{
+        pool.close;
+        sql.close;
+        console.log(err);
+  })
+})
 
-        queryDb("INSERT INTO GOALS VALUES(@_1, @_2, @_3, @_4, @_5, @_6, @_7)", goalinfo)
-        .catch(err=>{
-          pool.close
-          sql.close();
-          console.log(err);
-        })
+router.post("/comments/create", (req, res, next) => {
+  const goalid = req.params.goalid;
+  const empid = req.params.empid;
+  const description = req.params.description;
+  queryDb("SELECT MAX(commentid) as mgi from comments")
+      .then(result=>{
+        const commentid = result.mgi + 1;
+        const commentinfo = [commentid, goalid, empid, description];
+        queryDb("INSERT INTO COMMENTS VALUES(@_1, @_2, @_3, @_4)", commentinfo);
       })
       .catch(err=>{
         pool.close;
