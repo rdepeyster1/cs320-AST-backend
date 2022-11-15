95ef27dfaa13dab0dafde62d53de7538f50e8602
lam.tran <lam.tran@onemount.com>
2022-11-15 13:39:39 -0500

    test goalid and all goal

--- test.js
+++ test.js
@@ -1,13 +1,32 @@
 const request = require("supertest");
 const app = require("./app");
 
-describe("Test the root path", () => {
-  test("It should response the GET method", done => {
+describe("Test all the goal API", () => {
+  test("GET api with goal id", done => {
     request(app)
-      .get("/")
+      .get("/api/goals/get/1")
       .then(response => {
-        expect(response.statusCode).toBe(200);
+        // expect(response.statusCode).toBe(200);
+        console.log("abc")
+        console.log(response._body[0]);
+        object = response._body[0];
+        expect(object.GoalID).toBe(1);
+        expect(object.EmpID).toBe(1);
         done();
       });
   });
+
+  test("GET api with all goal from empID", done => {
+    request(app)
+      .get("/api/goals/getAllGoals/1")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        console.log(response);
+        object = response._body;
+        expect(Object.keys(object).length).toBe(4);
+        // expect(object.EmpID).toBe(1);
+        done();
+      });
+  });
+
 });
